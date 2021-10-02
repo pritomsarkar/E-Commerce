5ecff314a5d846a33621a02b38081dcb53b35f72
pritomsarkar <pritomsarkar1997@gmail.com>
2021-10-02 16:49:16 +0530

    "emplementing routes"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./components/screens/HomeScreen";
+import ProductScreen from "./components/screens/ProductScreen";
+import CartScreen from "./components/screens/CartScreen";
+import LoginScreen from "./components/screens/LoginScreen";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route path="/" component={HomeScreen} exact></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/cart/" component={CartScreen}></Route>
+          <Route path="/login/" component={LoginScreen}></Route>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
---
+++ frontend/src/components/screens/CartScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CartScreen = () => {
+  return (
+    <div>
+      <h3>cart</h3>
+    </div>
+  );
+};
+
+export default CartScreen;
