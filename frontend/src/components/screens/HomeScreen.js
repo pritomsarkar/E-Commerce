e9f7c55d76b06b5a6ff3b08af60edb308be82687
pritomsarkar <pritomsarkar1997@gmail.com>
2021-10-02 16:18:31 +0530

    "Products show dummy"

---
+++ frontend/src/components/screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+
+import Product from "../Product";
+
+import products from "../../products";
+
+const HomeScreen = () => {
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} el={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default HomeScreen;
