b25b8b0fefc09c21239b8b6e82ab0c6a2dbc6388
pritomsarkar <pritomsarkar1997@gmail.com>
2021-10-03 01:30:18 +0530

    "db connections"

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
@@ -23,5 +26,7 @@ app.get("/api/products/:id", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} at ${PORT}`);
+  console.log(
+    `Server running in ${process.env.NODE_ENV} at ${PORT}`.yellow.bold
+  );
 });
